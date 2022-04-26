a19b9a370ecdd8e426cae91bf85b1d5e4d1227ee
StoneTone <thetony3535@gmail.com>
2022-04-26 16:07:29 -0500

    Slider Brightness
    update to slider for brightness working on HTML DOC

--- Current/hue.py
+++ Current/hue.py
@@ -2,6 +2,7 @@ from phue import Bridge
 import random as r
 from time import sleep
 from flask import Flask
+from PIL import ImageColor
 from flask import render_template, request
 app = Flask(__name__)
 
@@ -15,21 +16,37 @@ having to use the numbers
 """
 lights = bridge.get_light_objects('name')
 
+#lights
 strip = lights["Light Strip"]
 out = lights["Outside Light"]
 
 """setting up the server"""
 @app.route("/", methods= ['GET', 'POST'])
-def button():
+def ButtonInput():
     if request.method == 'POST':
         if request.form.get('On'):
             strip.on = True
         if request.form.get('Off'):
             strip.on = False
-        brit = request.form['Brightness']
-        
     return render_template('app.html')
+
+@app.route("/uInput", methods= ['GET', 'POST'])        
+def uInput():
+    if request.method == 'POST':
+        if request.form.get('brightness'):
+            brit = request.form['brightness']
+            strip.brightness = int(brit)
+        if request.form.get('color'):
+            #red = pow((red + 0.055) / (1.0 + 0.055), 2.4) if red > 0.04045 else (red / 12.92)
+            #green = pow((green + 0.055) / (1.0 + 0.055), 2.4) if green > 0.04045 else (green / 12.92)
+            #blue =  pow((blue + 0.055) / (1.0 + 0.055), 2.4) if blue > 0.04045 else (blue / 12.92)
+            color = request.form.get('color')
+            RGB = ImageColor.getcolor(color, "RGB")
+            
+            
     
+    return render_template('app.html')
+
 if __name__ == "__main__":
     app.run()
 
